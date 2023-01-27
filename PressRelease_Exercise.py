75b06cf6fa1bcd92c605a7cdcef653607cd0b633
Vladislav Volkov <122274962+eighternal@users.noreply.github.com>
2023-01-27 19:27:56 +0700

    Exercise on string Methods
    Create a press release, change some names, delete some spaces, write everything in upper case

---
+++ PressRelease_Exercise.py
@@ -0,0 +1,13 @@
+press_release='''
+
+Doody Calls, a nationwide leader in dog poop removal
+services, is growing its footprint in the pooper scooper
+industry with an opening of an office in Orlando,
+Florida. Doody Calls currently cleans up dog poop in over
+57 territories across 23 states and has been named the
+number-one dog poop removal franchise in the United States
+by Enterpreneur Magazine`s  annual Franchise 500 list.
+
+'''
+
+print(press_release.strip().replace("dog poop", "pet waste").replace("Doody Calls","DoodyCalls").upper())
