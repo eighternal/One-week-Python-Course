eb5f991464a8993867fda5b8f401f9e625a00757
Vladislav Volkov <122274962+eighternal@users.noreply.github.com>
2023-01-30 16:16:16 +0700

    Add files via upload

---
+++ ifelse_learning.py
@@ -0,0 +1,15 @@
+age=int(input("Enter your age "))
+
+if age >= 21:
+    print("Welcome to our bar")
+    print("*"*30)
+elif age <= 21:
+    IQ = int(input("Okay, what`s 2+2? "))
+
+    if IQ == 4:
+        print("AHAAA, A SMART ONE, COME ON IN!!!!")
+        print("*"*30)
+    else:
+        print("U ST00PID")
+        print("*"*30)
+
