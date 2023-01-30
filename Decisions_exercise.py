f3ca5dd6275cc21f7dc832cc94f1bebb78fb9359
Vladislav Volkov <122274962+eighternal@users.noreply.github.com>
2023-01-30 15:50:48 +0700

    Complete Decisions exercise
    First time using if:else:

---
+++ Decisions_exercise.py
@@ -0,0 +1,11 @@
+print("Welcome to our great and excellent bar")
+print("-"*50)
+age=int(input("Hey you! Enter your age in digits to enter our AMAZING BEAUTIFUL BAR!!!!!!! "))
+
+if age >= 21:
+
+    print("WELCOME TO OUR GREAT AND EXCELLENT WONDERFUL MEGA BAR< YOU BIG LARGE STRONG MOOSE WARRIOR!!!!!!!")
+
+else:
+
+    print("NONONONONO! GET OUTTA HERE, YOU KIPPAH KILLER SLEEPING POOPING DIET COKE LALALA!!!!!!")
