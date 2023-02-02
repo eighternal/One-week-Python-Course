6a5e9adc5cbbd8faadd75072df9d01ac71614fd4
Vladislav Volkov <122274962+eighternal@users.noreply.github.com>
2023-02-02 16:25:56 +0700

    This is a WHILE loop exercise

---
+++ One_Week_Python_Exercises/snake_eyes_codealong.py
@@ -0,0 +1,14 @@
+# import random
+# roll1 = random.randint(1,6)
+# roll2 = random.randint(1,6)
+# roll_count = 1
+
+# while roll1 != 1 or roll2 != 1:
+#     print(roll1, roll2)
+#     roll1 = random.randint(1,6)
+#     roll2 = random.randint(1,6)
+#     roll_count +=1
+
+# print(roll1, roll2)
+# print(f"IT TOOK {roll_count} ATTEMPTS")
+# print("YAY, SNAKE EYES!!!")
